4ea376b8dfeb2ca9fb4559746e2a89e795ac92b7
aryan21710 <aryan21710@gmail.com>
2020-11-09 04:04:40 +0530

    CUSTOMHOOKS ARE MOVED TO SERVICES FOLDER

--- src/customHooks/index.js
+++
@@ -1,9 +0,0 @@
-import { useFetchDataFromBackend } from './useFetchDataFromBackend';
-import { usePostDataToFetchResult } from './usePostDataToFetchResult';
-import {useSelectedPlanetDataTOHandleAnim} from './useSelectedPlanetDataTOHandleAnim'
-
-export const myCustomHooks = {
-	useFetchDataFromBackend,
-	usePostDataToFetchResult,
-    useSelectedPlanetDataTOHandleAnim
-}
